2e60291deed434ee7c72db0d220012abb3500c63
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-01-26 23:31:54 +0700

    Update Cat and Mouse

--- Mouse.js
+++ Mouse.js
@@ -1,6 +1,11 @@
 function Mouse(name, color) {
 	this.name = name;
 	this.color = color;
+	this.dead = false;
+}
+
+Mouse.prototype.die = function() {
+	this.dead = true;
 }
 
 var mickey = new Mouse('mickey', 'black');
