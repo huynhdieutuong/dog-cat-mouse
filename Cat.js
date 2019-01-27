d7510e00735fb105dfe91d060e4d94ec17fcd8fd
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-01-27 10:12:30 +0700

    ok

--- Cat.js
+++ Cat.js
@@ -2,8 +2,11 @@ function Cat() {
 	this.stomach = [];
 }
 
+<<<<<<< HEAD
 Cat.prototype.eat = function(mouse) {
 	this.stomach.push(mouse);
 };
 
+=======
+>>>>>>> parent of 96430ea... Update Cat
 module.exports = Cat;
